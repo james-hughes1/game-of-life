2c30dc6d341959554f8f01ffb6d65b0e35f63a4d
James Hughes <jh2284@cam.ac.uk>
2023-11-17 16:59:55 +0000

    Setup conway package; wrote unit tests.

--- test/test_1.py
+++ test/test_1.py
@@ -1,5 +1,58 @@
-from src import main
+import numpy as np
+from src import conway
 
 
-def test_main():
-    assert main.func(2, 3) == 5
+# The following three tests are patterns taken from
+# https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns
+
+
+def test_loaf():
+    initial_state = np.array(
+        [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0],
+            [0, 1, 0, 0, 1, 0],
+            [0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ]
+    )
+    loaf = conway.board.Board(initial_state)
+    for _ in range(10):
+        loaf.step()
+    assert loaf.data == initial_state
+
+
+def test_beacon():
+    initial_state = np.array(
+        [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0],
+        ]
+    )
+    beacon = conway.board.Board(initial_state)
+    beacon.step()
+    assert beacon.data != initial_state
+    beacon.step()
+    assert beacon.data == initial_state
+
+
+def test_glider():
+    initial_state = np.array(
+        [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0, 0],
+            [0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ]
+    )
+    glider = conway.board.Board(initial_state)
+    for _ in range(24):
+        glider.step()
+    assert glider.data == initial_state
